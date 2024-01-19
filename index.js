4876bfc1ca179acdc8dda1e0520469385e3b948c
ddewaay <ddewaay@uiowa.edu>
2024-01-19 17:38:41 -0600

    Ready for deployment!

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("clicked from eventListener")
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = ""
 })
+
+
+function renderLeads() {
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length;i++) {
+        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    }
+
+    ulEl.innerHTML = listItems
+}
