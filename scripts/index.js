35cf95fc223e2a7aa90b8ff977f41ab72a2d807f
ddewaay <dondewaay@gmail.com>
2024-01-29 23:14:31 -0600

    Edit

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,36 @@
 /* DARK THEME */
 
-let darkMode = localStorage.getItem("darkMode")
-const darkModeToggle = document.getElementById("dark-mode-toggle")
-const darkLightDefault = window.matchMedia("(prefers-color-scheme: dark)")
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const darkLightDefault = window.matchMedia("(prefers-color-scheme: dark)");
 
 function enableDarkMode() {
-    document.body.classList.add("dark-theme")
-    localStorage.setItem("darkMode", "enabled")
-    darkModeToggle.innerHTML = "☀️"
+  document.body.classList.add("dark-theme");
+  localStorage.setItem("darkMode", "enabled");
+  darkModeToggle.innerHTML = "☀️";
 }
 
 function disableDarkMode() {
-    document.body.classList.remove("dark-theme")
-    localStorage.setItem("darkMode", "disabled")
-    darkModeToggle.innerHTML = "🌙"
+  document.body.classList.remove("dark-theme");
+  localStorage.setItem("darkMode", "disabled");
+  darkModeToggle.innerHTML = "🌑";
 }
 
-darkModeToggle.addEventListener('click', function() {
-    darkMode = localStorage.getItem("darkMode")
-    if (darkMode !== "enabled") {
-        enableDarkMode()
-    } else {
-        disableDarkMode()
-    }
-})
+darkModeToggle.addEventListener("click", function () {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
 
 if (darkMode === "enabled") {
-    enableDarkMode()
+  enableDarkMode();
 } else if (darkMode === "disabled") {
-    disableDarkMode()
+  disableDarkMode();
 } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    enableDarkMode()
+  enableDarkMode();
 }
 
 /* SCRIPTS */
